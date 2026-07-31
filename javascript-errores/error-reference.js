39feb134a0ef658eaa2ba38a6c8a138702284f0b
omiras <oscar.miras@gmail.com>
2026-07-31 10:50:16 +0200

    done

---
+++ javascript-errores/error-reference.js
@@ -0,0 +1,6 @@
+
+let Pepe = "holahola";
+// El programa se piensa que existe una variable que se llama Pepe
+let nombre = Pepe;
+
+console.log(nombre);
---
+++ javascript-errores/error-sintaxis.js
@@ -0,0 +1,5 @@
+let edad = 18;
+
+if (edad >= 18) {
+    console.log("Eres mayor de edad")
+// me dejo el cierra-llaves }
